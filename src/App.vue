b74fe1ed5cb4f15acf7236a1d1d84e78723ccb45
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-01-21 12:22:25 +0900

    add abi

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,27 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import abi from "@/abi/xtokensABI.js";
+import { ethers } from "ethers";
+const PRIVATE_KEY = import.meta.env.PRIVATE_KEY;
+
+const provider = new ethers.providers.JsonRpcProvider("https://rpc.api.moonbeam.network");
+const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+const xTokens = new ethers.Contract(
+  '0x0000000000000000000000000000000000000804',
+  abi,
+  signer
+);
+async function transferToAlice() {
+  const transaction = await xTokens.transfer(
+    xcUINIT_ADDRESS,
+    '1000000000000',
+    ALICE_RELAY_ACC,
+    '1000000000'
+  );
+  await transaction.wait();
+  console.log(transaction);
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <p>loaded</p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
